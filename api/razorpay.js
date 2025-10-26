00d5100ad4826d945262de3091e49d14ccbce82c
Aditya-yadav56 <amaraavathifocus@gmail.com>
2025-10-26 08:40:47 +0530

    razorpay.js

--- api/razorpay.js
+++ api/razorpay.js
@@ -7,8 +7,8 @@ app.use(cors());
 app.use(express.json());
 
 const razorpay = new Razorpay({
-  key_id: "rzp_test_RXvVfYiWIjkUTz",
-  key_secret: "INT7DetaMPM1tcjxN2TrUz9a",
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
 app.post("/create-order", async (req, res) => {
